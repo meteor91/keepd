8b125153e5121156216f1135bf0bc1f6c4edc7d9
meteor91 <bilalsir@yandex.ru>
2023-09-05 00:06:42 +0600

    dependencies test

--- rollup.config.js
+++ rollup.config.js
@@ -1,33 +1,32 @@
-import resolve from "@rollup/plugin-node-resolve";
-import commonjs from "@rollup/plugin-commonjs";
-import typescript from "@rollup/plugin-typescript";
-import terser from '@rollup/plugin-terser';
-import dts from "rollup-plugin-dts";
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import typescript from '@rollup/plugin-typescript';
+import dts from 'rollup-plugin-dts';
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 import postcss from 'rollup-plugin-postcss';
 
-import * as packageJson from "./package.json" assert {type: "json"};
+import * as packageJson from './package.json' assert {type: 'json'};
 
 export default [
     {
-        input: "src/index.ts",
+        input: 'src/index.ts',
         // TODO check external
-        external: ['react', 'react-dom'],
+        //external: ['react', 'react-dom'],
         output: [
             {
                 file: packageJson.main,
-                format: "cjs",
+                format: 'cjs',
                 sourcemap: true,
             },
             {
                 file: packageJson.module,
-                format: "esm",
+                format: 'esm',
                 sourcemap: true,
             },
 
             {
                 file: 'local/keepd/dist/esm/index.js',
-                format: "esm",
+                format: 'esm',
                 sourcemap: true,
             },
         ],
@@ -35,28 +34,28 @@ export default [
             peerDepsExternal(),
             resolve(),
             commonjs(),
-            typescript({tsconfig: "./tsconfig.json"}),
+            typescript({ tsconfig: './tsconfig.json' }),
             // terser(),
             postcss({
                 plugins: [],
                 use: {
                     less: {
                         paths: ['./src'],
-                    }
-                }
-            })
+                    },
+                },
+            }),
         ],
     },
     {
-        input: "dist/esm/types/index.d.ts",
-        output: [{file: "dist/index.d.ts", format: "esm"}],
+        input: 'dist/esm/types/index.d.ts',
+        output: [{ file: 'dist/index.d.ts', format: 'esm' }],
         plugins: [dts.default()],
         external: [/\.less$/],
     },
     {
-        input: "dist/esm/types/index.d.ts",
+        input: 'dist/esm/types/index.d.ts',
         output: [
-            {file: "local/keepd/dist/index.d.ts", format: "esm"}
+            { file: 'local/keepd/dist/index.d.ts', format: 'esm' },
         ],
         plugins: [dts.default()],
         external: [/\.less$/],
